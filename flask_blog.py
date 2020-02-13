0929e1a50c4373c9ce4ed349f21227c69b5760ba
Lee O'Connell <leeoconnell@Lees-MacBook-Pro.local>
2020-02-13 19:23:31 +0000

    init commit

---
+++ flask_blog.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# Fake database posts:
+posts = [
+    {
+        'author': 'Lee O\'Connell',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'John Smith',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    },
+    {
+        'author': 'John Smith',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
